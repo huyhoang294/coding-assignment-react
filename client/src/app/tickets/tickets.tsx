ba814c91603570447fde1623eba7478587e3a9f4
Hoàng Đỗ <hoangdo@Hoangs-MacBook-Pro.local>
2024-07-03 17:45:51 +0700

    install packages

--- client/src/app/tickets/tickets.tsx
+++ client/src/app/tickets/tickets.tsx
@@ -1,5 +1,6 @@
 import { Ticket } from '@acme/shared-models';
 import styles from './tickets.module.css';
+import { Input } from "@/components/ui/input"
 
 export interface TicketsProps {
   tickets: Ticket[];
@@ -9,6 +10,7 @@ export function Tickets(props: TicketsProps) {
   return (
     <div className={styles['tickets']}>
       <h2>Tickets</h2>
+      <Input />
       {props.tickets ? (
         <ul>
           {props.tickets.map((t) => (
